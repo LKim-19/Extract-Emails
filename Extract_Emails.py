6028d9838e77f5c28bbf4f4bd0b577ac719c54fe
Louis <47342278+LKim-19@users.noreply.github.com>
2023-06-04 22:54:57 -0700

    Update Extract_Emails.py
    Explanation of the code

--- Extract_Emails.py
+++ Extract_Emails.py
@@ -4,8 +4,12 @@ fname = "mbox-short.txt"
 
 fh = open(fname)
 
+# For loop to split the lines up into individual words
+# in the file as a list
 for line in fh:
     x=line.split()
+    # If From is in the list then it will print out the email which is the next word in the list also
+    # keeps count of how many emails there are
     if 'From' in x:
         print(x[1])
         count += 1
